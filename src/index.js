66a334af2c9d5371c1d5fa657223160ab546f711
hoannguyen02 <nguyenvanhoan02@gmail.com>
2020-06-04 10:47:31 +0700

    Change title

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import SelectExample from './examples/SelectExample';
 
 const App = () => (
   <>
-    <h2 style={{ textAlign: 'center' }}>Welcome to React UI Components</h2>
+    <h2 style={{ textAlign: 'center' }}>Welcome to Efficient UI</h2>
     <ModalExample />
     <SelectExample />
   </>
